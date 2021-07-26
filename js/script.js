6ba1e1621b62ce14f34317b11e5f4a987b08e0ab
LeoDias-0 <leodiasleodias00@gmail.com>
2021-07-25 21:47:48 -0300

    Conferir pedido

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-
-
 function handleClick(divItem) {
     const subsecaoAtual = divItem.parentNode
     const itemSelecionado = subsecaoAtual.querySelector('.selecionado')
-
-    const botaoFecharPedido = document.querySelector('.botao-fechar')
-    const botaoFecharTexto = botaoFecharPedido.querySelector('span')
 
     if (itemSelecionado === null) {
         divItem.classList.add('selecionado')
@@ -16,18 +11,70 @@ function handleClick(divItem) {
         divItem.classList.add('selecionado')
     }
 
-    const pedidoPronto = verificarPedido()
+    const pedidoPronto = verificarSePedidoEstaPronto()
 
-    if (pedidoPronto) {
-        botaoFecharPedido.classList.add('botao-verde')
-        botaoFecharTexto.innerHTML = 'Fechar pedido'
-    } else {
-        botaoFecharPedido.classList.remove('botao-verde')
-        botaoFecharTexto.innerHTML = 'Selecione os 3 itens<br>para fechar o pedido'
-    }
+    alterarBotaoConformePedido(pedidoPronto)
 }
 
-function verificarPedido() {
+function fecharPedido() {
+
+    const nomeComida = document.querySelector('.comida-selecionada').innerHTML
+    const nomeBebida = document.querySelector('.bebida-selecionada').innerHTML
+    const nomeSobremesa = document.querySelector('.sobremesa-selecionada').innerHTML
+
+    let precoTotal = document.querySelector('.preco-total').innerHTML
+
+    precoTotal = tratarPreco(precoTotal)
+
+    const mensagemPedido = construirMensagemPedido(nomeComida, nomeBebida, nomeSobremesa, precoTotal)
+
+    const numeroRestaurante = '5548984658779'
+
+    enviarMensagemWhatsapp(numeroRestaurante, mensagemPedido)
+}
+
+
+function confirmarPedido(){
+
+    const pedidoPronto = verificarSePedidoEstaPronto()
+
+    if (! pedidoPronto) return
+
+    const nomeComida = pegarNomeItemSelecionadoDaSubsecao('.comida')
+    const nomeBebida = pegarNomeItemSelecionadoDaSubsecao('.bebida')
+    const nomeSobremesa = pegarNomeItemSelecionadoDaSubsecao('.sobremesa')
+
+    let precoComida = pegarPrecoItemSelecionadoDaSubsecao('.comida')
+    let precoBebida = pegarPrecoItemSelecionadoDaSubsecao('.bebida')
+    let precoSobremesa = pegarPrecoItemSelecionadoDaSubsecao('.bebida')
+
+    let precoTotal = somarTotalPedido(precoComida, precoBebida, precoSobremesa)
+
+    precoTotal = 'R$ ' + precoTotal.replace('.', ',')
+
+    alterarTexto('.comida-selecionada', nomeComida)
+    alterarTexto('.preco-comida-selecionada', precoComida)
+
+    alterarTexto('.bebida-selecionada', nomeBebida)
+    alterarTexto('.preco-bebida-selecionada', precoBebida)
+
+    alterarTexto('.sobremesa-selecionada', nomeSobremesa)
+    alterarTexto('.preco-sobremesa-selecionada', precoSobremesa)
+
+    alterarTexto('.preco-total', precoTotal)
+
+    alterarTexto('.modal').classList.remove('oculto')
+}
+
+function voltarParaPaginaSelecao() {
+    document.querySelector('.modal').classList.add('oculto')
+}
+
+//  O---------------O
+//  | Funções Úteis |
+//  O---------------O
+
+function verificarSePedidoEstaPronto() {
     if ( !temItemSelecionado('.comida')) return false
     if ( !temItemSelecionado('.bebida')) return false
     if ( !temItemSelecionado('.sobremesa')) return false
@@ -39,39 +86,36 @@ function temItemSelecionado(cssSelectorSubsecao) {
     return subsecao.querySelector('.selecionado') !== null
 }
 
-function fecharPedido() {
-    const pedidoPronto = verificarPedido()
+function alterarTexto(cssSelector, texto) {
+    document.querySelector(cssSelector).innerHTML = texto
+}
 
-    if (! pedidoPronto) return
+function alterarBotaoConformePedido(pedidoPronto) {
+    const botaoFecharPedido = document.querySelector('.botao-fechar')
+    const botaoFecharTexto = botaoFecharPedido.querySelector('span')
 
-    const numeroRestaurante = '5548984658779'
-    const urlBase = `https://wa.me/${numeroRestaurante}?text=`
+    if (pedidoPronto) {
+        botaoFecharPedido.classList.add('botao-verde')
+        botaoFecharTexto.innerHTML = 'Fechar pedido'
+    } else {
+        botaoFecharPedido.classList.remove('botao-verde')
+        botaoFecharTexto.innerHTML = 'Selecione os 3 itens<br>para fechar o pedido'
+    }
+}
 
-    const nomeComida = pegarNomeItemSelecionadoDaSubsecao('.comida')
-    const nomeBebida = pegarNomeItemSelecionadoDaSubsecao('.bebida')
-    const nomeSobremesa = pegarNomeItemSelecionadoDaSubsecao('.sobremesa')
+function tratarPreco(stringPreco) {
+    const stringPrecoTratada = stringPreco.replace('R$ ', '').replace(',', '.')
+    return stringPrecoTratada
+}
 
-    let precoComida = pegarPrecoItemSelecionadoDaSubsecao('.comida')
-    let precoBebida = pegarPrecoItemSelecionadoDaSubsecao('.bebida')
-    let precoSobremesa = pegarPrecoItemSelecionadoDaSubsecao('.bebida')
-
+function somarTotalPedido(precoComida, precoBebida, precoSobremesa) {
     precoComida = tratarPreco(precoComida)
     precoBebida = tratarPreco(precoBebida)
     precoSobremesa = tratarPreco(precoSobremesa)
 
-    let precoTotal = precoComida + precoBebida + precoSobremesa
+    let precoTotal = Number(precoComida) + Number(precoBebida) + Number(precoSobremesa)
 
-    precoTotal = precoTotal.toFixed(2)
-
-    let mensagemPedido = `Olá, gostaria de fazer o pedido:
-    - Prato: ${nomeComida}
-    - Bebida: ${nomeBebida}
-    - Sobremesa: ${nomeSobremesa}
-    Total: R$ ${precoTotal}`
-
-    const urlCompleta = urlBase + encodeURIComponent(mensagemPedido)
-
-    window.open(urlCompleta)
+    return precoTotal.toFixed(2)
 }
 
 function pegarNomeItemSelecionadoDaSubsecao(cssSelectorSubsecao) {
@@ -85,10 +129,25 @@ function pegarPrecoItemSelecionadoDaSubsecao(cssSelectorSubsecao) {
     const itemSelecionado = document.querySelector(cssSelectorSubsecao)
 
     const precoItem = itemSelecionado.querySelector('.preco').innerHTML
-    return precoItem
+    return tratarPreco(precoItem)
 }
 
-function tratarPreco(stringPreco) {
-    const stringPrecoTratada = stringPreco.replace('R$ ', '').replace(',', '.')
-    return Number(stringPrecoTratada)
+function construirMensagemPedido(nomeComida, nomeBebida, nomeSobremesa, precoTotal) {
+    
+    const mensagemPedido = `Olá, gostaria de fazer o pedido:
+    - Prato: *${nomeComida}*
+    - Bebida: *${nomeBebida}*
+    - Sobremesa: *${nomeSobremesa}*
+    Total: *R$ ${precoTotal}*`
+
+    return mensagemPedido
+}
+
+function enviarMensagemWhatsapp(numero, mensagem) {
+
+    const urlBase = `https://wa.me/${numero}?text=`
+
+    const urlCompleta = urlBase + encodeURIComponent(mensagem)
+
+    window.open(urlCompleta)
 }
